cbf82423dfcc3bf2d47ecfd84ed531c720b62fae
Mustafa Raza <73385368+mustafaraza751137@users.noreply.github.com>
2021-03-23 16:07:46 +0530

    local storage

--- notes/script.js
+++ notes/script.js
@@ -1,4 +1,11 @@
+let count = Number(window.localStorage.getItem("count"));
+if (!count) {
+  window.localStorage.setItem("count", "0");
+}
+
 function createNote(noteTitle , noteBody){
+    document.getElementById("no-notes").classList.add("hidden");
+
     let li = document.createElement("li");
     let a = document.createElement("a");
     let h2 = document.createElement("h2");
@@ -30,9 +37,46 @@ function createNoteFromInput(e){
     let noteBody = document.getElementById("new-note-body-input").value;
     document.getElementById("new-note-title-input").value="";
     document.getElementById("new-note-body-input").value="";
+    
+    count += 1;
+    window.localStorage.setItem("count", count);
+    window.localStorage.setItem(noteTitle , noteBody);
+
     createNote(noteTitle , noteBody);
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains("delete")) {
+        if (confirm('Are you sure you wanna delete the note?')) {
+            let li = e.target.parentElement.parentElement;
+            let ul = document.getElementById("notes");
+
+            ul.removeChild(li);
+        }
+    }
+
+    count -= 1;
+    window.localStorage.setItem("count", count);
+    window.localStorage.removeItem(e.target.previousElementSibling.innerText);
+
+    if(count<1){
+        document.getElementById("no-notes").className="";
+    }
+}
+
+for(i=0;i<count+1;i++){
+    let  noteTitle= window.localStorage.key(i);
+    let  noteBody= window.localStorage.getItem(noteTitle);
+
+    if(noteTitle !== "count" && noteTitle){
+        createNote(noteTitle , noteBody);
+    }
 }
 
 document
     .getElementById("inputform")
     .addEventListener("submit",createNoteFromInput , false);
+
+document
+    .getElementById("notes")
+    .addEventListener("click", removeItem);
