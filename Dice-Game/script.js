c3227ab0f3d744ad645b533d7a50537f77c82ed2
Mustafa Raza <73385368+mustafaraza751137@users.noreply.github.com>
2021-10-12 22:51:34 +0530

    complete

--- Dice-Game/script.js
+++ Dice-Game/script.js
@@ -26,25 +26,34 @@ function get(p){
 
 // Function to roll the dice and match with input
 setInterval(function(){
-    setTimeout(function () {
-        var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-        document.querySelector(".img1").setAttribute("src",
-            "images/dice" + randomNumber + ".png");
-        
-        if (randomNumber == value) {
-            document.querySelector(".result").innerText = (player + ", Your guess was right!");
-            score+=10;
-            document.querySelector(".score span").innerText=score;
-        }
+    // Here max=6 and min=1;
+    // Math.floor(Math.random() * (max - min + 1) + min);
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-        else {
-            document.querySelector(".result").innerText
-                            = (player + ", Your guess was wrong!");
-        }
-        value=0;
-        btn.className="input";
-    }, 1000);
+    console.log(randomNumber);
+
+    // rolling dice
+    document.querySelector(".img1").setAttribute("src",
+        "images/dice" + randomNumber + ".png");
+    
+    // checking for result
+    if (randomNumber == value) {
+        document.querySelector(".result").innerText = (player + ", Your guess was right!");
+        score+=10;
+        document.querySelector(".score span").innerText=score;
+    }
+
+    else {
+        document.querySelector(".result").innerText
+                        = (player + ", Your guess was wrong!");
+    }
+
+    //changing value to its default value after matching result
+    value=0;
+    document.querySelector(".variable span").innerText=0;
+    btn.className="input";
+
 },4000);
 
 // Fuction for timer
@@ -63,4 +72,8 @@ setInterval(function(){
 // Function to change the player name
 function editName() {
     player = prompt("Change Player name");
+
+    // if given name is null or empty don't change
+    if(player == null || player == "")
+        player="Player";
 }
